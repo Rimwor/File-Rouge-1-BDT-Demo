1b6739b93a6140fc392898d9bdd60bcb676f0a7b
Flavie Féray <flavie.feray@gmail.com>
2021-08-02 00:35:18 +0200

    Version 3.0 (connexion réussie / échec + map utilisateurs)

--- JS/comptes_utilisateurs.js
+++ JS/comptes_utilisateurs.js
@@ -1,3 +1,5 @@
+//map des comptes utilisateur (en attendant la base de données)
+
 var users = [
     {type: "administrateur", identifiant: "2259873640", pin: "850326", name: "Marcel Farot"},
     {type: "bibliothécaire", identifiant: "6418097238", pin: "145637", name: "Andy Dufresne"},
@@ -5,3 +7,5 @@ var users = [
     {type: "responsable", identifiant: "7493501689", pin: "520378", name: "Louise Nouget"},
     {type: "gestionnaire", identifiant: "4468792031", pin: "097163", name: "Bertrand Guillon"}
 ];
+
+
---
+++ JS/error_connection.js
@@ -0,0 +1,46 @@
+
+// Déclaration de variables
+var btnOk;
+var tentatives;
+var nbEssais =3;
+var nbTentatives = 2;
+var messageErreur;
+
+/**
+ * function JQuery ==> initialiser les variables issues du doc html
+ * Flavie
+ */
+$(document).ready(function () {
+    btnOk = $("#btn_ok");
+    tentatives = $("#tentatives");
+    messageErreur = $("#statut_echec");
+
+    //L'utilisateur confirme la lecture du message d'erreur
+    btnOk.on("click", goToSignin);
+});
+
+setTimeout (tries, 100);
+
+/**
+ * incrémente le nombre de tentatives de connexion effectuées (PB ==> nombre de tentative remis à 0 à chaque chargement des pages ==> besoin du BACK END pour données de session ?);
+ * Affiche le nombre d'essais restants ==> OK
+ * Change le message concernant le statut du compte ==> compte bloqué si 0 essai restant ==> OK
+ * Flavie
+ */
+function tries () {
+    nbTentatives++;
+    nbEssais -= nbTentatives;
+    tentatives.html("<span> "+nbEssais+"</span>");
+    if (nbEssais <= 0) {
+        messageErreur.html("<p> Votre compte est désormais bloqué !</p><p>Veuillez contacter l'administateur.</p>");
+    }
+}
+
+/**
+ * renvoie à la page de connexion
+ * Flavie
+ */
+function goToSignin () {
+    window.location.replace("index.html");
+    // nbTentatives++;
+}
