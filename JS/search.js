99e4015011a4e1ba727c10efed29162ba8c35bd2
Beata Krzyżosiak <80414308+Rimwor@users.noreply.github.com>
2021-08-17 16:29:04 +0200

    search v0.1 very beta - Beata

--- JS/search.js
+++ JS/search.js
@@ -66,20 +66,20 @@
 
 function myFunction() {
     // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
+    var input, filter, td, tr, td, i, txtValue;
     input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
+    table = document.getElementById("myTab");
+    tr = ul.getElementsByTagName('tr');
   
     // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
+        tr[i].style.display = "";
       } else {
-        li[i].style.display = "none";
+        tr[i].style.display = "none";
       }
     }
   }
