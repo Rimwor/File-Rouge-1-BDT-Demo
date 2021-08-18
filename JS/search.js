cd34a1e07d8dd8d841024f19e4927913fe4346e2
Beata Krzyżosiak <80414308+Rimwor@users.noreply.github.com>
2021-08-18 10:37:00 +0200

    VERSION: search v3.1 // delete file : bd_list > all is in search.js now ^_^ // Beata

--- JS/search.js
+++ JS/search.js
@@ -1,44 +1,60 @@
-// ------------------------------------------------------------------------------------------- //
-/*
-var html = "<table border='1|1' class='tab1' id='myTab' >";
+var search_bar = document.getElementById("myInput");
+search_bar.addEventListener("keyup", mySearch_serie);
+
+function mySearch_serie() {
+  /**
+ * Table list
+ * @param {string}
+ * Beata
+ */
+ var html = "<table border='1|1' class='tab1' id='myTab' >";
 
  for(var [idSerie, serie] of series.entries()) {
      // Recherche des albums de la série - la list
      for (var [idAlbum, album] of albums.entries()) {
-         if (album.idSerie == idSerie) {
-           if (album.titre.includes("ast")) {
-             html+="<tr class='tr1'>";
-             html+="<td class='td1'>"+serie.nom+", Album N°"+album.numero+" "+album.titre+", Auteur : "+auteurs.get(album.idAuteur).nom+"</td>";
-             html+="</tr>";
-           }
-         }
+        if (album.idSerie == idSerie) {
+            if (serie.nom.includes(search_bar.value)) {
+              html+="<tr class='tr1'>";
+              html+="<td class='td1'>"+serie.nom+", Album N°"+album.numero+" "+album.titre+", Auteur : "+auteurs.get(album.idAuteur).nom+"</td>";
+              html+="</tr>";
+            }
+          }
      }
      
  }
  html+="</table>";
  console.log(html);
- document.getElementById("box_auteurs").innerHTML += html;
+ document.getElementById("box_auteurs").innerHTML = html;
 
-/**/
-
-function myFunction() {
-  // Declare variables
-  var input, filter, table, tr1, td1, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("tab1");
-  tr1 = table.getElementsByClassName("tr1");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr1.length; i++) {
-    td1 = tr1[i].getElementsByClassName("td1")[0];
-    if (td1) {
-      txtValue = td1.textContent || td1.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr1[i].style.display = "";
-      } else {
-        tr1[i].style.display = "none";
-      }
-    }
-  }
 }
+ 
+ /**
+  * An example for a table list  
+  */
+ 
+ // var rows = [{
+ //     name: "John",
+ //     age: 20,
+ //     email: "xx@hotmail.com"
+ // }, {
+ //     name: "Jack",
+ //     age: 50,
+ //     email: "xxx@hotmail.com"
+ // }, {
+ //     name: "Son",
+ //     age: 45,
+ //     email: "xxxx@hotmail.com"
+ // }];
+ 
+ // var html = "<table border='1|1'>";
+ // for (var i = 0; i < rows.length; i++) {
+ //     html+="<tr>";
+ //     html+="<td>"+rows[i].name+"</td>";
+ //     html+="<td>"+rows[i].age+"</td>";
+ //     html+="<td>"+rows[i].email+"</td>";
+     
+ //     html+="</tr>";
+ 
+ // }
+ // html+="</table>";
+ // document.getElementById("box").innerHTML = html;
