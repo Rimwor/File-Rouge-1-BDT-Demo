1c2da739f1fb683f40d4d22ff86aae9e9f0663ef
Beata Krzyżosiak <80414308+Rimwor@users.noreply.github.com>
2021-08-18 11:13:55 +0200

    VERSION: search v3.2 (par titre et par serie OK) // Beata

--- JS/search.js
+++ JS/search.js
@@ -23,7 +23,35 @@ function mySearch_serie() {
      
  }
  html+="</table>";
- console.log(html);
+ document.getElementById("box_auteurs").innerHTML = html;
+
+}
+
+var search_bar1 = document.getElementById("myInput1");
+search_bar1.addEventListener("keyup", mySearch_titre);
+
+function mySearch_titre() {
+  /**
+ * Table list
+ * @param {string}
+ * Beata
+ */
+ var html = "<table border='1|1' class='tab1' id='myTab' >";
+
+ for(var [idSerie, serie] of series.entries()) {
+     // Recherche des albums de la série - la list
+     for (var [idAlbum, album] of albums.entries()) {
+        if (album.idSerie == idSerie) {
+            if (album.titre.includes(search_bar1.value)) {
+              html+="<tr class='tr1'>";
+              html+="<td class='td1'>"+album.titre+", "+serie.nom+", Album N°"+album.numero+", Auteur : "+auteurs.get(album.idAuteur).nom+"</td>";
+              html+="</tr>";
+            }
+          }
+     }
+     
+ }
+ html+="</table>";
  document.getElementById("box_auteurs").innerHTML = html;
 
 }
